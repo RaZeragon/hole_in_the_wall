84ad592de0d746649e73847dbc267ee04a25137b
RaZeragon <kward3@stevens.edu>
2023-09-01 15:34:28 -0400

    Updated image processing script with new circle algorithm

--- hitw_algorithm/images/image_processing.py
+++ hitw_algorithm/images/image_processing.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 
 # TO DO
-# Test other versions of hole algorithm
 # Update to C++
 
 # Read original image
@@ -27,9 +26,6 @@ ret, thresh = cv2.threshold(edges, 127, 100, cv2.THRESH_BINARY)
 cv2.imshow('Threshold', thresh)
 cv2.waitKey(0)
 
-# Create image for first link mask
-link1_img = np.zeros((height, width), np.uint8)
-
 # Link length (m)
 # Test ratio: 300 pixels | 1 m
 link1_length_m = 0.5
@@ -37,128 +33,109 @@ link2_length_m = 0.5
 link1_length_pixels = int(link1_length_m * 300)
 link2_length_pixels = int(link2_length_m * 300)
 
-# Would be better to iterate over the coords of the circle?
-# Would need to figure out way of generating circle coords but would probably solve the most problems in the long run
-# Draw a circle over the link1_img with the radius = link length to simulate the joint path
 circle_center_x = int(width / 2)
 circle_center_y = height - 1
-circle_width = 2
-link1_img_circle = cv2.circle(link1_img, (circle_center_x, circle_center_y), link1_length_pixels, 100, circle_width)
 
-cv2.imshow('Circle', link1_img_circle)
-cv2.waitKey(0)
+def buildOctants(circle_center_x, circle_center_y, x, y, octants):
+    # Use the coordinates from first octant to fill in the rest of the circle
+    # because of circular symmetry
+    octants[0].append([circle_center_x + x, circle_center_y + y])
+    octants[1].append([circle_center_x + y, circle_center_y + x])
+    octants[2].append([circle_center_x + y, circle_center_y - x])
+    octants[3].append([circle_center_x + x, circle_center_y - y])
+    octants[4].append([circle_center_x - x, circle_center_y - y])
+    octants[5].append([circle_center_x - y, circle_center_y - x])
+    octants[6].append([circle_center_x - y, circle_center_y + x])
+    octants[7].append([circle_center_x - x, circle_center_y + y])
 
-# Adds the two images together to see where the joint intersects the hole outline via increased pixel value
-combined_images = thresh + link1_img_circle
+def orderOctants(octants):
+    # Orders the octants in a list of coordinates that form a continuous
+    # loop to allow for searching along the circle
+    circle_coordinates = []
 
-cv2.imshow('Combined', combined_images)
-cv2.waitKey(0)
+    for index in range(1, len(octants), 2):
+        quadrant = octants[index - 1] + octants[index][::-1]
+        del quadrant[-1]
+        circle_coordinates = circle_coordinates + quadrant
 
-# Create bounds for rectangle around the circle to iterate through the combined images
-offset = circle_width - 1
-lower_x = circle_center_x - link1_length_pixels - offset
-upper_x = circle_center_x + link1_length_pixels + offset
-lower_y = circle_center_y - link1_length_pixels - offset
-upper_y = circle_center_y
+    return circle_coordinates
 
-intersection_list = []
+def circleBres(circle_center_x, circle_center_y, radius):
+    # Generates coordinates for a circle of a specific center
+    # and radius using the Bresenham's circle algorithm
+    x = 0
+    y = radius
+    d = 3 - (2 * radius)
+    octants = [[], [], [], [], [], [], [], []]
+    buildOctants(circle_center_x, circle_center_y, x, y, octants)
 
-# Iterates through the rectangle to check for intersections and append them to a list
-# Again, would probably be better to just iterate over a circle
-for x_cord in range(lower_x, upper_x):
-    for y_cord in reversed(range(lower_y, upper_y)):
+    while (y >= x):
+        x = x + 1
 
-        # combined_images[y_cord, x_cord] = combined_images[y_cord, x_cord] + 50
-        if (combined_images[y_cord, x_cord] < 150):
-            continue
+        if (d > 0):
+            y = y - 1
+            d = d + 4 * (x - y) + 10
+        else:
+            d = d + 4 * x + 6
+        
+        if (y >= x):
+            buildOctants(circle_center_x, circle_center_y, x, y, octants)
 
-        intersection_list.append([x_cord, y_cord])
+    return(orderOctants(octants))
 
-# Next part assumes there were only two intersections
-# Need to tell which pixels are grouped together and choose the edge pixels of those groupings
-# Need to add error checking for edge cases
-previous_x_coord = intersection_list[0][0]
-starting_coordinate = []
-ending_coordinate = []
+def findIntersections(circle_coordinates, threshold_image):
+    # Iterates through the circle coordinates and tests if they're
+    # within the image. If they are, then check if that pixel is
+    # bright which means it is an edge of the hole and add that coordinate
+    # to a list of intersections
+    intersection_list = []
 
-for coordinate_id in range(1, len(intersection_list)):
-    if (intersection_list[coordinate_id][0] > (previous_x_coord + 1)):
-        ending_coordinate = intersection_list[coordinate_id]
-        starting_coordinate = intersection_list[coordinate_id - 1]
-        break
+    for coordinate in circle_coordinates:
+        try:
+            threshold_image[coordinate[1], coordinate[0]]
+        except:
+            pass
+        else:
+            if threshold_image[coordinate[1], coordinate[0]] > 50:
+                intersection_list.append(coordinate)
 
-    previous_x_coord = intersection_list[coordinate_id][0]
+    return intersection_list
 
-# Could change to just averaging the two coordinate points
-# Could also do similar triangles?
-starting_angle = math.atan2(starting_coordinate[1] - circle_center_y, circle_center_x - starting_coordinate[0]) * (-1)
-ending_angle = math.atan2(ending_coordinate[1] - circle_center_y, circle_center_x - ending_coordinate[0]) * (-1)
+def createAnglePairs(intersection_list):
+    pass
 
-first_angle_rad = (starting_angle + ending_angle) / 2
+test_coordinates = circleBres(circle_center_x, circle_center_y, link1_length_pixels)
+print(findIntersections(test_coordinates, thresh))
 
-# Calculate position of first joint
-joint_center_x = circle_center_x - (math.cos(first_angle_rad) * link1_length_pixels)
-joint_center_y = circle_center_y - (math.sin(first_angle_rad) * link1_length_pixels)
+# # Next part assumes there were only two intersections
+# # Need to tell which pixels are grouped together and choose the edge pixels of those groupings
+# # Need to add error checking for edge cases
+# previous_x_coord = intersection_list[0][0]
+# starting_coordinate = []
+# ending_coordinate = []
 
-# print(joint_center_x, joint_center_y)
-# combined_images[int(joint_center_y), int(joint_center_x)] = 255
+# for coordinate_id in range(1, len(intersection_list)):
+#     if (intersection_list[coordinate_id][0] > (previous_x_coord + 1)):
+#         ending_coordinate = intersection_list[coordinate_id]
+#         starting_coordinate = intersection_list[coordinate_id - 1]
+#         break
 
+#     previous_x_coord = intersection_list[coordinate_id][0]
 
-# Repeat joint tracing process with second joint
-link2_img = np.zeros((height, width), np.uint8)
-link2_img_circle = cv2.circle(link2_img, (int(joint_center_x), int(joint_center_y)), link2_length_pixels, 100, circle_width)
+# # Could change to just averaging the two coordinate points
+# # Could also do similar triangles?
+# starting_angle = math.atan2(starting_coordinate[1] - circle_center_y, circle_center_x - starting_coordinate[0]) * (-1)
+# ending_angle = math.atan2(ending_coordinate[1] - circle_center_y, circle_center_x - ending_coordinate[0]) * (-1)
 
-cv2.imshow('Second circle', link2_img_circle)
-cv2.waitKey(0)
+# first_angle_rad = (starting_angle + ending_angle) / 2
 
-combined_images = thresh + link2_img_circle
+# # Calculate position of first joint
+# joint_center_x = circle_center_x - (math.cos(first_angle_rad) * link1_length_pixels)
+# joint_center_y = circle_center_y - (math.sin(first_angle_rad) * link1_length_pixels)
 
-cv2.imshow('Second combined', combined_images)
-cv2.waitKey(0)
+# # print(joint_center_x, joint_center_y)
+# # combined_images[int(joint_center_y), int(joint_center_x)] = 255
 
-lower_x = int(joint_center_x) - link2_length_pixels - offset
-upper_x = int(joint_center_x) + link2_length_pixels + offset
-lower_y = int(joint_center_y) - link2_length_pixels - offset
-upper_y = int(joint_center_y) + link2_length_pixels + offset
-
-if (lower_x < 0):
-    lower_x = 0
-if (lower_y < 0):
-    lower_y = 0
-if (upper_x > 639):
-    upper_x = 639
-if (upper_y > 479):
-    upper_y = 479
-
-# print(lower_x, upper_x, lower_y, upper_y)
-
-intersection_list = []
-
-for x_cord in range(lower_x, upper_x):
-    for y_cord in reversed(range(lower_y, upper_y)):
-
-        # combined_images[y_cord, x_cord] = combined_images[y_cord, x_cord] + 50
-        if (combined_images[y_cord, x_cord] < 150):
-            continue
-
-        intersection_list.append([x_cord, y_cord])
-
-print(intersection_list)
-
-previous_x_coord = intersection_list[0][0]
-coordinate_list = []
-starting_coordinate = []
-ending_coordinate = []
-
-for coordinate_id in range(1, len(intersection_list)):
-    if (intersection_list[coordinate_id][0] > (previous_x_coord + 1)):
-        ending_coordinate = intersection_list[coordinate_id]
-        starting_coordinate = intersection_list[coordinate_id - 1]
-        coordinate_list.append([starting_coordinate, ending_coordinate])
-
-    previous_x_coord = intersection_list[coordinate_id][0]
-
-print(coordinate_list)
 
 cv2.imshow('Second combined square', combined_images)
 cv2.waitKey(0)
